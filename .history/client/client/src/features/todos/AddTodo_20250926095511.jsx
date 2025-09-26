cbb08657bad6840db1b79a5e1ab1e548584e4ae2
Hichem Cherki <164259725+ItsClaudeee4@users.noreply.github.com>
2025-09-26 10:27:54 +0100

    Delete .history directory

--- .history/client/client/src/features/todos/AddTodo_20250926095511.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { addTodos } from "./todoSlice";
-
-function AddTodo() {
-  return <div>AddTodo</div>;
-}
-
-export default AddTodo;
--- .history/server/routes/todoRoutes_20250918042628.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  getTodo,
-  createTodo,
-  updateTodo,
-  deleteTodo,
-} = require("../controllers/todoController");
